81e357d297394442a1681d5b77d204da6db13f52
Kamusubi <khephdjelidi@gmail.com>
2025-04-10 18:09:16 +0200

    test

---
+++ src/test.tsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect, createContext, useContext } from 'react';
+ import { WebSocketChatManager } from './component/WebSocketChatManager';
+ import { Chatter, RealChatter } from './component/Chat';
+ import { sharedChatManager } from './chatManager';
+ 
+ const Test = () => {
+   return (
+     <div>
+       <h1>Welcome to the Chat App</h1>
+       <RealChatter chatManager={sharedChatManager} />
+     </div>
+   );
+ };
+ 
+ export default Test;

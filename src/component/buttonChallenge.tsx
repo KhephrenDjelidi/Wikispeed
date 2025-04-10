e7faf531dd5acce17cb35843ae1cd2d98385868c
dnkee <147556640+dnkee@users.noreply.github.com>
2025-04-10 17:29:30 +0200

    boss

--- src/component/buttonChallenge.tsx
+++ src/component/buttonChallenge.tsx
@@ -5,23 +5,26 @@ type WebViewButtonProps = {
 };
 
 const WebViewButton: React.FC<WebViewButtonProps> = ({ onClick }) => {
-  const [isWebView, setIsWebView] = useState<boolean>(false);
-
-  useEffect(() => {
-    const webview = isInWebView();
-    setIsWebView(webview);
-    console.log('User Agent:', navigator.userAgent);
-    console.log('Is WebView:', webview);
-  }, []);
-
-  return (
-    <div>
-      <button disabled={!isWebView} onClick={onClick} className='button'>
-        {isWebView && 'CHALLENGE' }
-      </button>
-    </div>
-  );
-};
+    const [isWebView, setIsWebView] = useState<boolean>(false);
+  
+    useEffect(() => {
+      const webview = isInWebView();
+      setIsWebView(webview);
+      console.log('User Agent:', navigator.userAgent);
+      console.log('Is WebView:', webview);
+    }, []);
+  
+    if (!isWebView) return null; // ⬅️ Cache tout si pas en WebView
+  
+    return (
+      <div>
+        <button onClick={onClick} className="button">
+          CHALLENGE
+        </button>
+      </div>
+    );
+  };
+  
 
 function isInWebView(): boolean {
   const userAgent: string = navigator.userAgent || navigator.vendor || (window as any).opera;
